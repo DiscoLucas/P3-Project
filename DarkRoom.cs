4136fc329525c5f693e55a9d92089e10b57dfd36
Christian Randleff <54103424+DangelTheMangel@users.noreply.github.com>
2022-11-10 15:36:06 +0100

    stacking should work

--- DarkRoom.cs
+++ DarkRoom.cs
@@ -39,63 +39,108 @@ namespace P3_Project
             //nu bliver det kun gjordt på 1
             //første i listen
 
-            try
-            {
+            /*try
+            {*/
                 //ved ikke hvad k er
                 int k = 1;
-                int uniguenessThreashold = 3;
-                Mat img1 = GetMatFromSDImage((Image)targetImages[0].Clone());
-                Mat img2 = GetMatFromSDImage((Image)targetImages[1].Clone());
+            
+            Mat stakkedImage = new Mat();
+            VectorOfKeyPoint vkeypoints1 = new VectorOfKeyPoint();
+            ORB orb = new ORB(numberOfFeatures: 500, scaleFactor: 1.6f, nLevels: 12, fastThreshold: 15, edgeThreshold: 1);
+            Mat firstDescriptoir = new Mat();
+            Mat homography = null;
+            for (int i = 1; i < targetImages.Count; i++) {
+                if (i == 1) {
+                    stakkedImage = GetMatFromSDImage((Image)targetImages[0].Clone());
+                    orb.DetectAndCompute(stakkedImage, null, vkeypoints1, firstDescriptoir, false);
+                }
+                Size size = stakkedImage.Size;
+                Mat img2 = GetMatFromSDImage((Image)targetImages[i].Clone());
 
-
-                ORB orb = new ORB(numberOfFeatures: 1500, scaleFactor: 1.6f, nLevels: 12, fastThreshold: 15, edgeThreshold: 1);
-
-
-                VectorOfKeyPoint vkeypoints1 = new VectorOfKeyPoint();
                 VectorOfKeyPoint vkeypoints2 = new VectorOfKeyPoint();
-                Mat outputImg1 = new Mat();
 
-
-                Mat firstDescriptoir = new Mat();
                 Mat secondDescriptoir = new Mat();
-                orb.DetectAndCompute(img1, null, vkeypoints1, firstDescriptoir, false);
+                
                 orb.DetectAndCompute(img2, null, vkeypoints2, secondDescriptoir, false);
 
                 VectorOfVectorOfDMatch maches = new VectorOfVectorOfDMatch();
-                BFMatcher machter = new BFMatcher(DistanceType.Hamming, crossCheck: true);
+                BFMatcher machter = new BFMatcher(DistanceType.Hamming, crossCheck: false);
                 machter.KnnMatch(firstDescriptoir, secondDescriptoir, maches, k, null);
-                Debug.WriteLine("machter ");
-                Mat mask = new Mat(maches.Size, 1, Emgu.CV.CvEnum.DepthType.Cv8U, 1);
+                Mat mask = new Mat(img2.Size, Emgu.CV.CvEnum.DepthType.Cv8U, 1);
+                //Mat mask = img2.Clone()
                 mask.SetTo(new MCvScalar(255));
-                Debug.WriteLine("mask ");
+                Debug.Write("\n ABE ABE ABE ABE: \n" + mask.IsEmpty + " " + mask.NumberOfChannels +" " + mask.Depth + " "+ mask.Size.Width+" x " + mask.Size.Height+ "\n");
                 
-                Mat result = new Mat();
-                Debug.WriteLine(vkeypoints1.Length + " " + vkeypoints2.Length + " " + maches.Size);
-                Features2DToolbox.DrawMatches(img1, vkeypoints1, img2, vkeypoints2, maches, result,new MCvScalar(100,100,230), new MCvScalar(0, 255, 20),null);
-                Debug.WriteLine("result ");
+                //CvInvoke.Decolor(mask, img2.Clone(), null);
+                Features2DToolbox.VoteForUniqueness(maches, 0.8, mask);
+                int nonZeroPix = CvInvoke.CountNonZero(mask);
+
+                //Mat result = new Mat();
+                //Debug.WriteLine(vkeypoints1.Length + " " + vkeypoints2.Length + " " + maches.Size);
+                //Features2DToolbox.DrawMatches(img1, vkeypoints1, img2, vkeypoints2, maches, result,new MCvScalar(100,100,230), new MCvScalar(0, 255, 20),null);
+
                 
-                outputImage = result;
-                for (int i = 0; i < maches.Size; i++) {
-                    for (int j = 0; j < maches[i].Size; j++) {
-                        Debug.WriteLine("index: " + i +"j index: "+j +" \ndistance: "+ maches[i][j].Distance 
-                            + " \nTrainIdx " + maches[i][j].TrainIdx 
-                            + " \nQueryIdx " + maches[i][j].QueryIdx
-                            + " \n first keypoint: " + vkeypoints1[maches[i][j].TrainIdx].Point
-                            + " \n second keypoint: " + vkeypoints2[maches[i][j].QueryIdx].Point
-                            );
+
+                //remove the points that are two uniqe
+                //Features2DToolbox.VoteForUniqueness(maches, Uniqueness, mask);
+                
+            if (nonZeroPix >= 4)
+                {
+                    
+                    nonZeroPix = Features2DToolbox.VoteForSizeAndOrientation(vkeypoints1, vkeypoints2, maches, mask, 1.5, 20);
+                    
+                    if (nonZeroPix >= 4) { 
+                        //reshape points 
+                        homography = Features2DToolbox.GetHomographyMatrixFromMatchedFeatures(vkeypoints2, vkeypoints1, maches, mask, 0.5);
+                        Mat warpedImage = new Mat();
+                        CvInvoke.WarpPerspective(img2, warpedImage, homography, size);
+
+                        //CvInvoke.WarpPerspective(img2, warpedImage, homography, size);
+                        //stakkedImage = drawStaggedImage(homography, stakkedImage);
+                        if (i == 1) { 
+                            stakkedImage /= targetImages.Count;
+                        }
+                        stakkedImage = ((warpedImage/ targetImages.Count) + stakkedImage);
+                       // outputImage = warpedImage;
                     }
                     
                 }
-                
             }
-            catch (Exception ex)
-            {
-                Debug.WriteLine(ex.Message);
-            }
+            //stakkedImage /= targetImages.Count;
+            /*stakkedImage = (255 * stakkedImage);*/
+
+            outputImage = stakkedImage;
 
 
-            }
-        
+            /*for (int i = 0; i < maches.Size; i++) {
+                for (int j = 0; j < maches[i].Size; j++) {
+                    Debug.WriteLine("index: " + i +"j index: "+j +" \ndistance: "+ maches[i][j].Distance 
+                        + " \nTrainIdx " + maches[i][j].TrainIdx 
+                        + " \nQueryIdx " + maches[i][j].QueryIdx
+                        + " \n first keypoint: " + vkeypoints1[maches[i][j].TrainIdx].Point
+                        + " \n second keypoint: " + vkeypoints2[maches[i][j].QueryIdx].Point
+                        );
+                }
+
+            }*/
+
+
+
+            /* }
+             catch (Exception ex)
+             {
+                 Debug.WriteLine(ex.Message);
+             }*/
+
+
+        }
+
+        private Mat drawStaggedImage(Mat homography, Mat stakkedImage)
+        {
+
+            return stakkedImage;
+        }
+
         /// <summary>
         /// stolen from https://stackoverflow.com/questions/40384487/system-drawing-image-to-emgu-cv-mat
         /// </summary>
