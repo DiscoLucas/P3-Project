a5988c100a6ec1204d3e581f51075cc47ee3c8a6
DiscoLucas <47451042+DiscoLucas@users.noreply.github.com>
2022-11-14 11:30:25 +0100

    User settings are now display settings

--- UserSettingsForm.Designer.cs
+++ UserSettingsForm.Designer.cs
@@ -29,34 +29,68 @@
         private void InitializeComponent()
         {
             this.cudaCheckBox = new System.Windows.Forms.CheckBox();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.saveSettings = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // cudaCheckBox
             // 
             this.cudaCheckBox.AutoSize = true;
-            this.cudaCheckBox.Location = new System.Drawing.Point(350, 129);
+            this.cudaCheckBox.Location = new System.Drawing.Point(3, 3);
             this.cudaCheckBox.Name = "cudaCheckBox";
             this.cudaCheckBox.Size = new System.Drawing.Size(152, 24);
             this.cudaCheckBox.TabIndex = 0;
             this.cudaCheckBox.Text = "Use GPU (cuda)";
             this.cudaCheckBox.UseVisualStyleBackColor = true;
+            this.cudaCheckBox.CheckedChanged += new System.EventHandler(this.cudaCheckBox_CheckedChanged);
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 1;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.tableLayoutPanel1.Controls.Add(this.cudaCheckBox, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.saveSettings, 0, 1);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 89.41878F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 10.58122F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(800, 426);
+            this.tableLayoutPanel1.TabIndex = 1;
+            this.tableLayoutPanel1.Paint += new System.Windows.Forms.PaintEventHandler(this.tableLayoutPanel1_Paint);
+            // 
+            // saveSettings
+            // 
+            this.saveSettings.AutoSize = true;
+            this.saveSettings.Location = new System.Drawing.Point(3, 383);
+            this.saveSettings.Name = "saveSettings";
+            this.saveSettings.Size = new System.Drawing.Size(75, 30);
+            this.saveSettings.TabIndex = 1;
+            this.saveSettings.Text = "Save";
+            this.saveSettings.UseVisualStyleBackColor = true;
+            this.saveSettings.Click += new System.EventHandler(this.saveSettings_Click);
             // 
             // UserSettingsForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.cudaCheckBox);
+            this.ClientSize = new System.Drawing.Size(800, 470);
+            this.Controls.Add(this.tableLayoutPanel1);
             this.Name = "UserSettingsForm";
             this.Text = "UserSettingsForm";
             this.Load += new System.EventHandler(this.UserSettingsForm_Load);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.CheckBox cudaCheckBox;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Button saveSettings;
     }
 }
--- UserSettingsForm.cs
+++ UserSettingsForm.cs
@@ -12,17 +12,40 @@ namespace P3_Project
 {
     public partial class UserSettingsForm : Form
     {
+        // Based on tutorial here: https://youtu.be/tIOWI0JBFkg
         public UserSettingsForm()
         {
             InitializeComponent();
         }
-
+        /// <summary>
+        /// Runs the method for returning the settings
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void UserSettingsForm_Load(object sender, EventArgs e)
+        {
+            GetSettings();
+        }
+        /// <summary>
+        /// Sets the elements on the settings form to be equal to the persistent settings,
+        /// from Settings.settings
+        /// </summary>
         public void GetSettings()
+        {
+            cudaCheckBox.Checked = Properties.Settings.Default.useGPU;
+        }
+        
+        private void tableLayoutPanel1_Paint(object sender, PaintEventArgs e)
         {
 
         }
 
-        private void UserSettingsForm_Load(object sender, EventArgs e)
+        private void saveSettings_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void cudaCheckBox_CheckedChanged(object sender, EventArgs e)
         {
 
         }
